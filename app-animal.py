5e23e54984fe5d644047ffdd537be823eba18ae0
Henrique Sousa <henrique.sousa@br.experian.com>
2023-10-22 20:08:19 -0300

    Implementação CRUD

--- app-animal.py
+++ app-animal.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import uuid4
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-   return {'msg': 'tudo certo!'}
+class Animal(BaseModel):
+   id: Optional[str] = None
+   nome: str
+   idade: int
+   sexo: str
+   cor: str
+
+banco: List[Animal] = []
+
+@app.get('/animais')
+def listar_animais():
+   return banco
+
+@app.get('/animais/{animal_id}')
+def obter_animal(animal_id):
+   for animal in banco:
+      if animal.id == animal_id:
+         return animal
+   return {'erro': 'Animal não localizado'}
+
+@app.post('/animais')
+def criar_animal(animal: Animal):
+   animal.id = str(uuid4())
+   banco.append(animal)
+   return None
+
+
+@app.delete('/animais/{animal_id}')
+def remover_animal(animal_id):
+   for index, animal in enumerate(banco):
+      if animal.id == animal_id:
+         del banco[index]
+         return {'msg': 'Animal removido com sucesso'}
+   return {'erro': 'Animal não localizado'}
