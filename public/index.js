4937c5674e20d1901c911976dcee7954428c3c13
Henrique Sousa <henrique.sousa@br.experian.com>
2023-10-24 17:24:40 -0300

    Frontend V1

--- public/index.js
+++ public/index.js
@@ -2,42 +2,78 @@ async function carregarAnimais(){
    const response = await axios.get('http://127.0.0.1:8000/animais');
    const animais = response.data;
    const lista = document.getElementById('lista_animais');
-   if(lista.innerHTML!==""){
+   lista.innerHTML = "";
+   for (const animal of animais) {
+      const buttonExcluir = document.createElement('button');
+      buttonExcluir.textContent = "Excluir";
+      buttonExcluir.onclick = function(event) {
+         excluiAnimal(event.target.parentNode.id);
+      }
+      const buttonAbrir = document.createElement('button');
+      buttonAbrir.textContent = "Visualizar";
+      buttonAbrir.onclick = function(event) {
+         visualizaAnimal(event.target.parentNode.id);
+      }
       const item = document.createElement('li');
-      const animal = animais.pop();
-      const linha = `${animal.nome}:\n idade: ${animal.idade}\n cor: ${animal.cor}\n sexo: ${animal.sexo}`
+      item.id = animal.id;
+      const linha = `${animal.nome}`
       item.innerText = linha;
       lista.appendChild(item);
-   } else{
-      for (const animal of animais) {
-         const item = document.createElement('li');
-         const linha = `${animal.nome}:\n\tidade: ${animal.idade}\n\tcor: ${animal.cor}\n\tsexo: ${animal.sexo}`
-         item.innerText = linha;
-         lista.appendChild(item);
-      }
+      item.appendChild(buttonAbrir);
+      item.appendChild(buttonExcluir);
    }
 }
 
 async function manipularFormulario(){
    const formulario = document.getElementById('form_animal');
    const input_nome = document.getElementById('nome');
+   const input_sexo = document.getElementById('sexo');
+   const input_idade = document.getElementById('idade');
+   const input_cor = document.getElementById('cor');
 
    formulario.onsubmit = async (event)=> {
       event.preventDefault();
       const nome_animal = input_nome.value;
+      const nome_sexo = input_sexo.value;
+      const nome_idade = input_idade.value;
+      const nome_cor = input_cor.value;
 
       await axios.post('http://127.0.0.1:8000/animais', {
          nome: nome_animal,
-         idade: 4,
-         sexo: "Femea",
-         cor: "Branco"
+         idade: nome_idade,
+         sexo: nome_sexo,
+         cor: nome_cor
       })
       carregarAnimais();
-      alert(`Animal ${nome_animal} Cadastrado`); 
    }
    
 }
 
+async function excluiAnimal(id){
+   const urlDoAnimal = `http://127.0.0.1:8000/animais/${id}`
+   await axios.delete(urlDoAnimal);
+   console.log(urlDoAnimal);
+   carregarAnimais();
+}
+
+async function visualizaAnimal(id){
+   carregarAnimais();
+   const urlDoAnimal = `http://127.0.0.1:8000/animais/${id}`
+   const response = await axios.get(urlDoAnimal);
+   const animal = response.data;
+   const sublista = document.createElement('ul');
+   const item = document.getElementById(`${id}`);
+   item.appendChild(sublista);
+   const sexo = document.createElement('li');
+   const idade = document.createElement('li');
+   const cor = document.createElement('li');
+   sexo.innerText = `Sexo: ${animal.sexo}`;
+   idade.innerText = `Idade: ${animal.idade} anos`;
+   cor.innerText = `Cor: ${animal.cor}`;
+   sublista.appendChild(sexo);
+   sublista.appendChild(idade);
+   sublista.appendChild(cor);
+}
 
 async function app(){
    console.log('App Iniciada');
